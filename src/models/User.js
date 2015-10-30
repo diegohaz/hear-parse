1182f97105790bae9851934a511e7ff23c5c5d49
Diego Haz <hazdiego@gmail.com>
2015-10-30 18:45:12 -0200

    Add fields in User class fix #5

--- src/models/User.js
+++ src/models/User.js
@@ -7,11 +7,14 @@ export default class User extends Parse.Object {
 
 	// schematize
 	schematize() {
-		this.get('name') 			 || this.set('name', '');
-		this.get('picture') 	 || this.set('picture', '');
-		this.get('service') 	 || this.set('service', {name: 'itunes'});
-		this.get('identified') || this.set('identified', false);
-    this.get('genres')     || this.set('genres', []);
+		this.get('name') 			   || this.set('name', '');
+		this.get('picture') 	   || this.set('picture', '');
+		this.get('service') 	   || this.set('service', {name: 'itunes'});
+		this.get('identified')   || this.set('identified', false);
+    this.get('genres')       || this.set('genres', []);
+    this.get('removedSongs') || this.set('removedSongs', []);
+    this.get('country')      || this.set('country', 'US');
+    this.get('locale')       || this.set('locale', 'en');
 	}
 
 	// beforeSave
@@ -23,13 +26,37 @@ export default class User extends Parse.Object {
 		response.success();
 	}
 
-	// get service
+	// get currentService
 	static get currentService() {
 		let user = Parse.User.current();
 		let service = user? user.get('service') : 'itunes';
 
 		return new Service(service? service.name : null)
 	}
+
+  // get removedSongs
+  static get removedSongs() {
+    let user = Parse.User.current();
+    let removedSongs = user? user.get('removedSongs') : [];
+
+    return removedSongs;
+  }
+
+  // get country
+  static get country() {
+    let user = Parse.User.current();
+    let country = user? user.get('country') : 'US';
+
+    return country;
+  }
+
+  // get locale
+  static get locale() {
+    let user = Parse.User.current();
+    let locale = user? user.get('locale') : 'en';
+
+    return locale;
+  }
 }
 
 Parse.Object.registerSubclass('_User', User);
