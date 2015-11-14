d38a2c322e97a40fd0e08012677e9746868f83e4
Diego Haz <hazdiego@gmail.com>
2015-11-14 19:43:32 -0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import User from './models/User';
-import Song from './models/Song';
-import PlacedSong from './models/PlacedSong';
-import Genre from './models/Genre';
 import Artist from './models/Artist';
+import Beacon from './models/Beacon';
+import Genre from './models/Genre';
 import Place from './models/Place';
+import PlacedSong from './models/PlacedSong';
+import Song from './models/Song';
 
 Parse.Cloud.beforeSave('_User', User.beforeSave);
 Parse.Cloud.beforeSave('Artist', Artist.beforeSave);
+Parse.Cloud.beforeSave('Beacon', Beacon.beforeSave);
 Parse.Cloud.beforeSave('Genre', Genre.beforeSave);
+Parse.Cloud.beforeSave('PlacedSong', PlacedSong.beforeSave);
 Parse.Cloud.beforeSave('Song', Song.beforeSave);
 Parse.Cloud.afterSave('Song', Song.afterSave);
-Parse.Cloud.beforeSave('PlacedSong', PlacedSong.beforeSave);
 
 Parse.Cloud.define('fetchPlace', function(request, response) {
   let latitude = +request.params.latitude;
@@ -44,8 +46,9 @@ Parse.Cloud.define('placeSong', function(request, response) {
   let serviceId = request.params.serviceId;
   let latitude = +request.params.latitude;
   let longitude = +request.params.longitude;
+  let beaconUUID = request.params.beaconUUID;
 
   let location = new Parse.GeoPoint(latitude, longitude);
 
-  PlacedSong.place(serviceId, location).then(response.success, response.error);
+  PlacedSong.place(serviceId, location, beaconUUID).then(response.success, response.error);
 });
